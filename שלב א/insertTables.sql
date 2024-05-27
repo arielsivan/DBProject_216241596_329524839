84aa3b977274cbc71ff430666c86a0a3bbf2fbf0
Ariel Sivan <49596453+trhtk242@users.noreply.github.com>
2024-05-27 12:56:59 +0300

    Add files via upload

---
+++ שלב א/insertTables.sql
@@ -0,0 +1,6 @@
+INSERT INTO SOLDIERS (sID, firstName, lastName, draftDate, releaseDate) VALUES
+(100001, 'John', 'Doe', TO_DATE('2015-01-10', 'YYYY-MM-DD'), TO_DATE('2020-01-10', 'YYYY-MM-DD')),
+(100002, 'Jane', 'Smith', TO_DATE('2016-02-15', 'YYYY-MM-DD'), TO_DATE('2021-02-15', 'YYYY-MM-DD')),
+(100003, 'Robert', 'Brown', TO_DATE('2017-03-20', 'YYYY-MM-DD'), TO_DATE('2022-03-20', 'YYYY-MM-DD')),
+(100004, 'Emily', 'Davis', TO_DATE('2018-04-25', 'YYYY-MM-DD'), TO_DATE('2023-04-25', 'YYYY-MM-DD')),
+(100005, 'Michael', 'Wilson', TO_DATE('2019-05-30', 'YYYY-MM-DD'), TO_DATE('2024-05-30', 'YYYY-MM-DD'));
